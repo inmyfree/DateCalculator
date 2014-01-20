5542e4338408f127df1fc5aa4461048c7b62661e
inmyfree <onmyfree1990@gmail.com>
2014-01-20 09:40:17 +0800

    Initial commit

---
+++ public/javascripts/admin.js
@@ -0,0 +1,36 @@
+function month_1()
+{
+    document.getElementById("massage_box").style.display='block';
+    document.getElementById("mask").style.visibility='visible';
+    document.getElementById("massage_box").style.visibility='visible';
+}
+function day_1()
+{
+    document.getElementById("massage_box_1").style.display='block';
+    document.getElementById("mask_1").style.visibility='visible';
+    document.getElementById("massage_box_1").style.visibility='visible';
+}
+function month_2()
+{
+    document.getElementById("massage_box_2").style.display='block';
+    document.getElementById("mask_2").style.visibility='visible';
+    document.getElementById("massage_box_2").style.visibility='visible';
+}
+function day_2()
+{
+    document.getElementById("massage_box_3").style.display='block';
+    document.getElementById("mask_3").style.visibility='visible';
+    document.getElementById("massage_box_3").style.visibility='visible';
+}
+function month_3()
+{
+    document.getElementById("massage_box_4").style.display='block';
+    document.getElementById("mask_4").style.visibility='visible';
+    document.getElementById("massage_box_4").style.visibility='visible';
+}
+function day_3()
+{
+    document.getElementById("massage_box_5").style.display='block';
+    document.getElementById("mask_5").style.visibility='visible';
+    document.getElementById("massage_box_5").style.visibility='visible';
+}
